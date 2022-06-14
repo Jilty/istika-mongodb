4b0f7b5dcc00bc0ab96e85e7706bbb498cb04890
Jilty <49185299+Jilty@users.noreply.github.com>
2022-06-14 10:24:18 +0530

    Update git.sh

--- Scripts/git.sh
+++ Scripts/git.sh
@@ -1,18 +1,25 @@
 #! bin/bash 
-pm2 --version 2>&1 >/dev/null
-PM2_IS_AVAILABLE=$?
-if [ $PM2_IS_AVAILABLE -eq 0 ];
-then
-  sudo apt-get install pm2
-fi
-
 git --version 2>&1 >/dev/null
 GIT_IS_AVAILABLE=$?
 # ...
 if [ $GIT_IS_AVAILABLE -eq 0 ];
 then
   sudo apt-get install git
-fi  
+fi 
+
+node --version 2>&1 >/dev/null
+NODE_IS_AVAILABLE=$?
+if [ $NODE_IS_AVAILABLE -eq 0 ];
+then
+  sudo apt-get install nodejs
+fi
+
+pm2 --version 2>&1 >/dev/null
+PM2_IS_AVAILABLE=$?
+if [ $PM2_IS_AVAILABLE -eq 0 ];
+then
+  npm install pm2 -g
+fi
 
 
 
