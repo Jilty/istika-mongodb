c1be40c8a29e0f737101dbc77a75df785e77aafe
Jilty <49185299+Jilty@users.noreply.github.com>
2022-06-14 10:11:24 +0530

    Update git.sh

--- Scripts/git.sh
+++ Scripts/git.sh
@@ -7,3 +7,11 @@ if [ $GIT_IS_AVAILABLE -eq 0 ];
 then
   sudo apt-get install git
 fi  
+
+pm2 --version
+PM2_IS_AVAILABLE=$?
+if [ $PM2_IS_AVAILABLE -eq 0 ];
+then
+  sudo apt-get install pm2
+fi
+
