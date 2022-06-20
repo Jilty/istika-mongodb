94c4372e7c85af08f6c37bbeaf196f2177a199d4
Jilty <49185299+Jilty@users.noreply.github.com>
2022-06-20 18:52:46 +0530

    Update mongo.sh

--- Scripts/mongo.sh
+++ Scripts/mongo.sh
@@ -6,8 +6,8 @@ then
   curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add - 2>&1 >/dev/null
   apt-key list
   echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee etc/apt/sources.list.d/mongodb-org-4.4.list
-  sudo apt update
-  sudo apt install mongodb-org
+  sudo apt-get update
+  sudo apt-get install mongodb-org
   sudo systemctl start mongod.service
   sudo systemctl status mongod
 fi
@@ -21,7 +21,7 @@ then
   sudo apt install apt-transport-https ca-certificates curl software-properties-common -y whatever
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-  sudo apt update
+  sudo apt-get update
   sudo apt-cache policy docker-ce 2>/dev/null | grep packages | cut -d '.' -f 1
   sudo apt-get install docker-ce 2>/dev/null | grep packages | cut -d '.' -f 1
  fi 
