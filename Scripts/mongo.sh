d5d85d3de0be49160bfe38cd3e5ee9e09cd3f118
Jilty <49185299+Jilty@users.noreply.github.com>
2022-06-20 12:38:01 +0530

    Update mongo.sh

--- Scripts/mongo.sh
+++ Scripts/mongo.sh
@@ -1,9 +1,7 @@
 mongo --version 2>&1 >/dev/null
 MONGO_IS_AVAILABLE=$?
 echo Checking for MongoDB VERSION: $MONGO_IS_AVAILABLE
-if [ $NODE_IS_AVAILABLE -eq 127 ];
+if [ $MONGO_IS_AVAILABLE -eq 127 ];
 then
-  curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - > /dev/null
-  sudo apt-get install -y nodejs > /dev/null
-  sudo apt-get install npm
+  echo mongo not available
 fi
