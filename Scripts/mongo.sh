1224e95ee0364f7fad1c663a639ee048921e3f9a
Jilty <49185299+Jilty@users.noreply.github.com>
2022-06-20 12:50:27 +0530

    Update mongo.sh

--- Scripts/mongo.sh
+++ Scripts/mongo.sh
@@ -3,5 +3,10 @@ MONGO_IS_AVAILABLE=$?
 echo Checking for MongoDB VERSION: $MONGO_IS_AVAILABLE
 if [ $MONGO_IS_AVAILABLE -eq 127 ];
 then
-  echo mongo not available
+  curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+  echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee etc/apt/sources.list.d/mongodb-org-4.4.list
+  sudo apt update
+  sudo apt install mongodb-org
+  sudo systemctl start mongod.service
+  sudo systemctl status mongod
 fi
