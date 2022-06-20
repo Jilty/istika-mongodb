e9e9d67dc949a86ad5655303e0e7ab5e9578506e
Jilty <49185299+Jilty@users.noreply.github.com>
2022-06-20 17:02:46 +0530

    Update mongo.sh

--- Scripts/mongo.sh
+++ Scripts/mongo.sh
@@ -20,10 +20,10 @@ then
   sudo apt update
   sudo apt install apt-transport-https ca-certificates curl software-properties-common -y whatever
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-  sudo apt update
+  add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+  apt update
   apt-cache policy docker-ce 2>/dev/null | grep packages | cut -d '.' -f 1
-  sudo apt install docker-ce 2>/dev/null | grep packages | cut -d '.' -f 1
+  apt install docker-ce 2>/dev/null | grep packages | cut -d '.' -f 1
  fi 
   
   
