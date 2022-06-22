1638fb5cc92d6770e1a1719714a73bbde4a94bc1
Jilty <49185299+Jilty@users.noreply.github.com>
2022-06-22 08:55:37 +0530

    Update mongo.sh

--- Scripts/mongo.sh
+++ Scripts/mongo.sh
@@ -1,17 +1,17 @@
 #! bin/bash
-# mongod --version 2>&1 >/dev/null
-# MONGO_IS_AVAILABLE=$?
-# echo Checking for MongoDB VERSION: $MONGO_IS_AVAILABLE
-# if [ $MONGO_IS_AVAILABLE -eq 127 ];
-# then
-#   curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add - 2>&1 >/dev/null
-#   apt-key list
-#   echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee etc/apt/sources.list.d/mongodb-org-4.4.list
-#   sudo apt-get update
-#   sudo apt-get install mongodb-org
-#   sudo systemctl start mongod.service
-#   sudo systemctl status mongod
-# fi
+mongod --version 2>&1 >/dev/null
+MONGO_IS_AVAILABLE=$?
+echo Checking for MongoDB VERSION: $MONGO_IS_AVAILABLE
+if [ $MONGO_IS_AVAILABLE -eq 127 ];
+then
+  curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add - 2>&1 >/dev/null
+  apt-key list
+  echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee etc/apt/sources.list.d/mongodb-org-4.4.list
+  sudo apt-get update
+  sudo apt-get install mongodb-org
+  sudo systemctl start mongod.service
+  sudo systemctl status mongod
+fi
 
 docker -v 2>&1 >/dev/null
 DOCKER_IS_AVAILABLE=$?
