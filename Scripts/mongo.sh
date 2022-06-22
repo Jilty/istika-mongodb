6a2fef906f6d224277fa66dac9361b35df2e2963
Jilty <49185299+Jilty@users.noreply.github.com>
2022-06-22 09:29:28 +0530

    Update mongo.sh

--- Scripts/mongo.sh
+++ Scripts/mongo.sh
@@ -1,16 +1,63 @@
 #! bin/bash
-mongod --version 2>&1 >/dev/null
-MONGO_IS_AVAILABLE=$?
-echo Checking for MongoDB VERSION: $MONGO_IS_AVAILABLE
-if [ $MONGO_IS_AVAILABLE -eq 127 ];
-then
-  sudo curl -fssl https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add - 2>&1 >/dev/null
-  apt-key list - 2>&1 >/dev/null
-  echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee etc/apt/sources.list.d/mongodb-org-4.4.list - 2>&1 >/dev/null
-  sudo apt-get update - 2>&1 >/dev/null
-  sudo apt-get install -y mongodb-org - 2>&1 >/dev/null
-  sudo systemctl start mongod.service - 2>&1 >/dev/null
-  sudo systemctl status mongod
+# mongod --version 2>&1 >/dev/null
+# MONGO_IS_AVAILABLE=$?
+# echo Checking for MongoDB VERSION: $MONGO_IS_AVAILABLE
+# if [ $MONGO_IS_AVAILABLE -eq 127 ];
+# then
+#   sudo curl -fssl https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add - 2>&1 >/dev/null
+#   apt-key list - 2>&1 >/dev/null
+#   echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee etc/apt/sources.list.d/mongodb-org-4.4.list - 2>&1 >/dev/null
+#   sudo apt-get update - 2>&1 >/dev/null
+#   sudo apt-get install -y mongodb-org - 2>&1 >/dev/null
+#   sudo systemctl start mongod.service - 2>&1 >/dev/null
+#   sudo systemctl status mongod
+# fi
+
+if [ -f /usr/bin/mongod ]; then
+  echo "MongoDB is installed on your machine."
+  kill -0 "$$" || exit
+else
+  echo "********************* MongoDB 4.0 INSTALL **************************"
+  echo "MongoDB is is not installed."
+  echo "You will be prompted several time for input during the install."
+  echo "******************************************************************"
+  
+  wget -qO- https://www.mongodb.org/static/pgp/server-4.0.asc | sudo bash -c "apt-key add"
+  
+  sudo bash -c "echo deb http://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse > /etc/apt/sources.list.d/mongodb-org.list"
+  sudo bash -c "apt update && apt upgrade -y"
+  sudo bash -c "apt install mongodb-org -y"
+
+  sudo bash -c "apt update && apt upgrade -y"
+  sudo bash -c "apt autoremove && apt clean"
+  
+  sudo bash -c " mkdir /data /data/db"
+    
+  sudo bash -c "ufw allow proto tcp from any to any port 27017" #recommend 'from any' to local network range
+  sudo bash -c "ufw enable"  
+  
+  sudo bash -c "systemctl enable mongod"  #enables Mongo on system startup
+  sudo bash -c "service mongod start"
+
+  echo "Username for your Mongo DB admin account (DONOT use ampersands -> @)?"
+  read mongo_admin_user
+  
+  echo "What is the password you want to set for $mongo_admin_user (DONOT use ampersands -> @)?"
+  read mongo_admin_pwd 
+  
+  echo "User name will be set to $mongo_admin_user and password to $mongo_admin_pwd."
+  
+  mongo "admin" --eval "db.createUser({'user':'$mongo_admin_user','pwd':'$mongo_admin_pwd','roles': ['userAdminAnyDatabase','readWriteAnyDatabase']})"  
+  
+  sudo bash -c "echo ' ' >> /etc/mongod.conf"
+  sudo bash -c "echo 'security:' >> /etc/mongod.conf"
+  sudo bash -c "echo '  authorization: enabled' >> /etc/mongod.conf"    
+  
+  sudo bash -c "service mongod restart"
+
+  #this is just a sanity check to ensure everything worked
+  mongo "yourproject-com" -u $mongo_admin_user -p --authenticationDatabase admin --eval "db.createCollection('dummy')"
+  mongo -u $mongo_admin_user -p --authenticationDatabase admin --eval "db.createUser({'user':'yourproject-web-user','pwd':'passw0rd123','roles':[{'role':'dbOwner','db':'yourproject-com'}]})"
 fi
 
 docker -v 2>&1 >/dev/null
