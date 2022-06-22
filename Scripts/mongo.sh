4d79ec9694452073d99768c6a134731ad63f3933
Jilty <49185299+Jilty@users.noreply.github.com>
2022-06-22 10:25:14 +0530

    Update mongo.sh

--- Scripts/mongo.sh
+++ Scripts/mongo.sh
@@ -19,8 +19,7 @@ if [ -f /usr/bin/mongod ]; then
 else
   echo "********************* MongoDB 4.0 INSTALL **************************"
   echo "MongoDB is is not installed."
-  echo "You will be prompted several time for input during the install."
-  echo "******************************************************************"
+  
   
   wget -qO- https://www.mongodb.org/static/pgp/server-4.0.asc | sudo bash -c "apt-key add"
   
@@ -28,13 +27,13 @@ else
   sudo bash -c "apt update && apt upgrade -y"
   sudo bash -c "apt install mongodb-org -y"
 
-  sudo bash -c "apt update && apt upgrade -y"
+#   sudo bash -c "apt update && apt upgrade -y"
   sudo bash -c "apt autoremove && apt clean"
   
-  sudo bash -c " mkdir /data /data/db"
+#   sudo bash -c " mkdir /data /data/db"
     
-  sudo bash -c "ufw allow proto tcp from any to any port 27017" #recommend 'from any' to local network range
-  sudo bash -c "ufw enable"  
+#   sudo bash -c "ufw allow proto tcp from any to any port 27017" #recommend 'from any' to local network range
+#   sudo bash -c "ufw enable"  
   
   sudo bash -c "systemctl enable mongod"  #enables Mongo on system startup
   sudo bash -c "service mongod start"
